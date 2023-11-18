3417454592a310eb22da0518c7eacf56421c5ff6
choyunhee <xornd1110@naver.com>
2023-11-18 14:48:52 +0900

    로그아웃 버튼 추가

--- lib/tab/mypage.dart
+++ lib/tab/mypage.dart
@@ -4,6 +4,8 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../Recipe/RecipeDetailPage.dart';
+import '../login/kakao_login.dart';
+import '../login/main_view_model..dart';
 
 class Mypage extends StatefulWidget {
   Mypage({Key? key}) : super(key: key);
@@ -15,6 +17,7 @@ class Mypage extends StatefulWidget {
 class _SearchState extends State<Mypage> {
   String? baseurl = dotenv.env['API_BASE_URL'];
   final storage = FlutterSecureStorage();
+  final viewModel = MainViewModel(KakaoLogin()); //카카오 로그인 객체 전달
   var _user = {};
   List<dynamic> _userBookmarked = [];
   var mypage_menu = ElevatedButton.styleFrom(
@@ -36,7 +39,7 @@ class _SearchState extends State<Mypage> {
     );
 
     if (response.statusCode == 200) {
-      Map responseData =  jsonDecode(utf8.decode(response.bodyBytes));
+      Map responseData = jsonDecode(utf8.decode(response.bodyBytes));
       bool isSuccess = responseData['isSuccess'];
       print('responseData ${responseData}');
       if (isSuccess) {
@@ -52,7 +55,6 @@ class _SearchState extends State<Mypage> {
   }
 
   Future<void> getUserBookmarked() async {
-    final storage = FlutterSecureStorage();
     String? jwtToken = await storage.read(key: 'jwt');
 
     if (jwtToken == null) {
@@ -81,162 +83,215 @@ class _SearchState extends State<Mypage> {
     }
   }
 
+  //로그인 여부 확인
+  isLoggedIn() async {
+    String? jwtToken = await storage.read(key: 'jwt');
+    // user의 정보가 있다면 첫 페이지로 이동
+    if (jwtToken == null) {
+      Navigator.pushNamed(context, '/login');
+    } else {
+      print('토큰있음');
+    }
+  }
+
+  logout() async {
+    await viewModel.logout();
+    storage.delete(key: 'jwt');
+    Navigator.pushReplacementNamed(context,
+        '/login');
+  }
+
   @override
   void initState() {
     super.initState();
     getUserData();
     getUserBookmarked();
+    // 비동기로 flutter secure storage 정보를 불러오는 작업
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      isLoggedIn();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 500.0,
-              child: Stack(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                        horizontal: 15.0, vertical: 15.0),
-                    height: 240.0,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/cover.jpg'),
-                          fit: BoxFit.cover),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ), //Container의 스타일 및 배경 이미지
-                  ),
-                  Positioned(
-                    top: 180,
-                    left: 0.0,
-                    right: 0.0,
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          backgroundImage: AssetImage('assets/profile.jpg'),
-                          radius: 70.0,
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        Text(_user['nickname'] ?? '',
-                            style: TextStyle(
-                                fontSize: 24.0, fontWeight: FontWeight.bold)),
-                        Text('학생',
-                            style: TextStyle(
-                                fontSize: 13.0, fontWeight: FontWeight.bold)),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.location_on),
-                            Text('부산광역시, 남구',
-                                style: TextStyle(
-                                    fontSize: 13.0, fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Text(
-                                '프로필 수정',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Color(0xFF242760),
-                                  minimumSize: Size(150, 50),
-                                  elevation: 0.0,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10.0))),
-                            ),
-                            SizedBox(width: 20), // 간격을 조절하기 위한 SizedBox 위젯 추가
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Text(
-                                '친구 추가',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Color(0xFF242760),
-                                  minimumSize: Size(150, 50),
-                                  elevation: 0.0,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10.0))),
-                            ),
-                          ],
-                        )
-                      ],
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          PopupMenuButton(
+              icon: Icon(Icons.more_vert, color:Colors.grey[600]),
+              itemBuilder: (BuildContext buildContext) {
+                return [
+                PopupMenuItem(child: TextButton(
+                  onPressed: (){
+                    logout();
+                  },
+                  child: Text(
+                    '로그아웃',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
                     ),
+                  ),
+                ),
+                )
+                ];
+              })
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 500.0,
+                child: Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                          horizontal: 15.0, vertical: 15.0),
+                      height: 240.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/cover.jpg'),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ), //Container의 스타일 및 배경 이미지
+                    ),
+                    Positioned(
+                      top: 180,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: AssetImage('assets/profile.jpg'),
+                            radius: 70.0,
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(_user['nickname'] ?? '',
+                              style: TextStyle(
+                                  fontSize: 24.0, fontWeight: FontWeight.bold)),
+                          Text('학생',
+                              style: TextStyle(
+                                  fontSize: 13.0, fontWeight: FontWeight.bold)),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.location_on),
+                              Text('부산광역시, 남구',
+                                  style: TextStyle(
+                                      fontSize: 13.0,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  '프로필 수정',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color(0xFF242760),
+                                    minimumSize: Size(150, 50),
+                                    elevation: 0.0,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            10.0))),
+                              ),
+                              SizedBox(width: 20), // 간격을 조절하기 위한 SizedBox 위젯 추가
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  '친구 추가',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color(0xFF242760),
+                                    minimumSize: Size(150, 50),
+                                    elevation: 0.0,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            10.0))),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+                child: Divider(),
+              ),
+              Column(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.local_grocery_store,
+                      color: Color(0xFF242760),
+                    ),
+                    label: Text(
+                      '내가 사용한 레시피',
+                      style: TextStyle(
+                          color: Color(0xFF242760),
+                          fontWeight: FontWeight.bold),
+                    ),
+                    style: mypage_menu,
+                  ),
+                  SizedBox(height: 10), // 간격을 조절하기 위한 SizedBox 위젯 추가
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.book,
+                      color: Color(0xFF242760),
+                    ),
+                    label: Text(
+                      '내가 사용한 레시피',
+                      style: TextStyle(
+                          color: Color(0xFF242760),
+                          fontWeight: FontWeight.bold),
+                    ),
+                    style: mypage_menu,
+                  ),
+                  SizedBox(height: 10), // 간격을 조절하기 위한 SizedBox 위젯 추가
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.settings,
+                      color: Color(0xFF242760),
+                    ),
+                    label: Text(
+                      '계정 관리',
+                      style: TextStyle(
+                          color: Color(0xFF242760),
+                          fontWeight: FontWeight.bold),
+                    ),
+                    style: mypage_menu,
                   ),
                 ],
               ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
-              child: Divider(),
-            ),
-            Column(
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.local_grocery_store,
-                    color: Color(0xFF242760),
-                  ),
-                  label: Text(
-                    '내가 사용한 레시피',
-                    style: TextStyle(
-                        color: Color(0xFF242760), fontWeight: FontWeight.bold),
-                  ),
-                  style: mypage_menu,
-                ),
-                SizedBox(height: 10), // 간격을 조절하기 위한 SizedBox 위젯 추가
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.book,
-                    color: Color(0xFF242760),
-                  ),
-                  label: Text(
-                    '내가 사용한 레시피',
-                    style: TextStyle(
-                        color: Color(0xFF242760), fontWeight: FontWeight.bold),
-                  ),
-                  style: mypage_menu,
-                ),
-                SizedBox(height: 10), // 간격을 조절하기 위한 SizedBox 위젯 추가
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.settings,
-                    color: Color(0xFF242760),
-                  ),
-                  label: Text(
-                    '계정 관리',
-                    style: TextStyle(
-                        color: Color(0xFF242760), fontWeight: FontWeight.bold),
-                  ),
-                  style: mypage_menu,
-                ),
-              ],
-            ),
-          Container(
-              margin: EdgeInsets.all(15.0),
-              child: TabBarScreen(userBookmarked: _userBookmarked))
-          ],
+              Container(
+                  margin: EdgeInsets.all(15.0),
+                  child: TabBarScreen(userBookmarked: _userBookmarked))
+            ],
+          ),
         ),
       ),
     );
@@ -246,13 +301,15 @@ class _SearchState extends State<Mypage> {
 class TabBarScreen extends StatefulWidget {
   final List userBookmarked;
 
-  TabBarScreen({Key? key, required this.userBookmarked}) : super(key: key); //recipe라는 이름의 매개변수를 받는 생성자
+  TabBarScreen({Key? key, required this.userBookmarked})
+      : super(key: key); //recipe라는 이름의 매개변수를 받는 생성자
 
   @override
   State<TabBarScreen> createState() => _TabBarScreenState();
 }
 
-class _TabBarScreenState extends State<TabBarScreen> with SingleTickerProviderStateMixin {
+class _TabBarScreenState extends State<TabBarScreen>
+    with SingleTickerProviderStateMixin {
   late TabController tabController = TabController(
     length: 2,
     vsync: this,
@@ -292,6 +349,7 @@ class _TabBarScreenState extends State<TabBarScreen> with SingleTickerProviderSt
         fontSize: 15,
         fontWeight: FontWeight.bold,
       ),
+
       /// 기본 인디캐이터의 컬러
       indicatorColor: Color(0xFF242760),
       indicatorWeight: 2,
@@ -334,11 +392,12 @@ class _RecipesState extends State<Recipes> {
         crossAxisSpacing: 7.0,
       ),
       scrollDirection: Axis.vertical,
-      itemBuilder: (context, i){
+      itemBuilder: (context, i) {
         return InkWell(
           onTap: () {
             print(widget.userBookmarked[i]);
-            Navigator.push(context, MaterialPageRoute(builder: (context) => RecipeDetailPage(recipe: widget.userBookmarked[i]),),);
+            Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                RecipeDetailPage(recipe: widget.userBookmarked[i]),),);
           },
           child: Image.network(
             widget.userBookmarked[i]['attFileNoMk'],
@@ -397,13 +456,13 @@ class FoodItems extends StatelessWidget {
         crossAxisSpacing: 7.0,
       ),
       scrollDirection: Axis.vertical,
-      itemBuilder: (context, i){
+      itemBuilder: (context, i) {
         return InkWell(
           onTap: () {
 
           },
           child: ClipRRect(
-            child: Image.network(//나중에 asset으로 변경
+            child: Image.network( //나중에 asset으로 변경
               fooditems[i]['imageUrl'],
               width: 100,
               height: 100,
